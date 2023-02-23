13dd2f2cad1c7f4eee38d7d857f7c3806710c5a3
Elizabeth Koning <ekoning2@dt-login01.delta.internal.ncsa.edu>
2023-02-23 16:04:02 -0600

    added delta submit script

---
+++ c_code/run_mpi_delta.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=sim    # Job name
+#SBATCH --time=02:00:00                 # Time limit hrs:min:sec
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=run_mpi_%j.log   # Standard output and error log
+#SBATCH --partition cpu
+#SBATCH --account bbkf-delta-cpu
+
+echo Tasks = ${SLURM_NTASKS}
+echo Nodes = ${SLURM_JOB_NUM_NODES}
+
+mpirun ./mpi_sim 512 1 test_mpi_0_
+
