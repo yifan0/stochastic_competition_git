7b15e579a144b0be60fb2b212b14e473e19f08de
kodingkoning <koningelizabeth@gmail.com>
2023-02-24 14:49:25 -0600

    updated run_simple

--- c_code/run_simple.sh
+++ c_code/run_simple.sh
@@ -7,13 +7,5 @@
 #SBATCH --output=simple_%j.log   # Standard output and error log
 #SBATCH --partition cs
 
-module load boost/1.71.0
-module load intel/18.0
-
-cat /home/ekoning2/stochastic_competition_git/c_code/simple_sim.cpp
-
-export OMP_NUM_THREADS=1
-echo "Threads = " $OMP_NUM_THREADS
-make clean && make
-/usr/bin/time -v /home/ekoning2/stochastic_competition_git/c_code/simple 512 1 test9.txt
+/usr/bin/time -v /home/ekoning2/stochastic_competition_git/c_code/simple 256 1 test11.txt
 
