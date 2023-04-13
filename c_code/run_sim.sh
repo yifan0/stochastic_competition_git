fe4783e10ba31f01a43218495a1b63e50f2fa23f
kodingkoning <koningelizabeth@gmail.com>
2023-04-13 09:56:54 -0500

    change seq sbatch to create heatmap

--- c_code/run_sim.sh
+++ c_code/run_sim.sh
@@ -7,7 +7,11 @@
 #SBATCH --output=logs/sim_%j.log   # Standard output and error log
 #SBATCH --partition eng-instruction
 
-SIZE=900
-
-/usr/bin/time -v ./build/sim -s ${SIZE} -o test_sequential_${SIZE}_
+SIZE=500
+OUTPUT=test_sequential_${SIZE}_
+module unload anaconda/2022-May/3
+cd build && make && cd ..
+/usr/bin/time -v ./build/sim -s ${SIZE} -o ${OUTPUT}
+module load anaconda/2022-May/3
+python heatmap.py ${OUTPUT}_rep0.csv
 
