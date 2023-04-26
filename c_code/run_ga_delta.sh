389add3a4f841212e1ff6509b49c918d6daad51e
Elizabeth Koning <ekoning2@dt-login01.delta.internal.ncsa.edu>
2023-04-26 13:33:26 -0500

    changed to 121 proc/node for delta

--- c_code/run_ga_delta.sh
+++ c_code/run_ga_delta.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ga_sim    # Job name
 #SBATCH --time=01:00:00                 # Time limit hrs:min:sec
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=121
 #SBATCH --output=logs/test_ga_%j.log   # Standard output and error log
 #SBATCH --partition cpu
 #SBATCH --account bbkf-delta-cpu
