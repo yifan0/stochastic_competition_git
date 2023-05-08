64467fc66309d975941990942ff160dd255c81f2
kodingkoning <koningelizabeth@gmail.com>
2023-05-08 17:28:17 -0500

    added making heatmaps of ga checkpoints

--- c_code/run_ga_cc.sh
+++ c_code/run_ga_cc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=ga_sim    # Job name
-#SBATCH --time=01:00:00                 # Time limit hrs:min:sec
+#SBATCH --time=04:00:00                 # Time limit hrs:min:sec
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=1
 #SBATCH --output=logs/test_ga_%j.log   # Standard output and error log
 #SBATCH --partition eng-instruction 
 
@@ -10,12 +10,17 @@ module unload anaconda/2022-May/3
 module load openmpi/4.1.0-gcc-7.2.0-pmi2
 
 SIZE=500
-OUTPUT=test_ga_${SIZE}_${SLURM_JOB_NUM_NODES}_${SLURM_NTASKS}
+OUTPUT=ga_test_for_mixing_${SIZE}_${SLURM_JOB_NUM_NODES}_${SLURM_NTASKS}
 echo Tasks = ${SLURM_NTASKS}
 echo Nodes = ${SLURM_JOB_NUM_NODES}
 
-mpirun ./build/ga_sim -s ${SIZE} -o ${OUTPUT}
+mpirun ./build/ga_sim -s ${SIZE} -o ${OUTPUT} --specrate=1e-7 
 
 module load anaconda/2022-May/3
+for i in {0..99..1}
+do
+    python heatmap.py ${OUTPUT}_rep0_checkpoint${i}.csv
+done
+
 python heatmap.py ${OUTPUT}_rep0.csv
 
