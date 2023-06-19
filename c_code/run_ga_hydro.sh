a21f65d90474ec13c79a3c90a904f1e85dadc5b7
kodingkoning <koningelizabeth@gmail.com>
2023-06-19 13:23:45 -0500

    fix printing of num tasks

--- c_code/run_ga_hydro.sh
+++ c_code/run_ga_hydro.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=ga_sim    # Job name
-#SBATCH --time=01:00:00                 # Time limit hrs:min:sec
-#SBATCH --nodes=1
+#SBATCH --time=7-00:00:00                 # Time limit hrs:min:sec
+#SBATCH --nodes=16
 #SBATCH --ntasks-per-node=16
 #SBATCH --output=logs/test_ga_%j.log   # Standard output and error log
 #SBATCH --partition sandybridge
 
-SIZE=500
-OUTPUT=ga_sim_${SIZE}_${SLURM_JOB_NUM_NODES}_${SLURM_NTASKS}
-echo Tasks = ${SLURM_NTASKS}
+SIZE=1000
+OUTPUT=ga_sim_${SIZE}_${SLURM_JOB_NUM_NODES}_${SLURM_NTASKS_PER_NODE}
+echo Tasks = ${SLURM_NTASKS_PER_NODE}
 echo Nodes = ${SLURM_JOB_NUM_NODES}
 
 module load openmpi/4.1.4
