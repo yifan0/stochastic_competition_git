8e1f7373c873a3c483cdf537c51938359f6e3d46
kodingkoning <koningelizabeth@gmail.com>
2023-10-04 11:42:02 -0500

    script to run GA no tree

---
+++ c_code/run_ga_no_tree_hydro.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=ga_sim    # Job name
+#SBATCH --time=0-07:20:00                 # Time limit hrs:min:sec
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --output=logs/test_ga_%j.log   # Standard output and error log
+#SBATCH --partition sandybridge
+
+SIZE=1000
+OUTPUT=ga_sim_${SIZE}_${SLURM_JOB_NUM_NODES}_${SLURM_NTASKS}
+echo Tasks = ${SLURM_NTASKS}
+echo Nodes = ${SLURM_JOB_NUM_NODES}
+
+echo "Running GA Sim without generating tree"
+module load openmpi/4.1.4
+srun ./build/ga_sim_no_tree -s ${SIZE} -o ${OUTPUT}
+
+# module load anaconda3_cpu/4.13.0
+# python heatmap.py ${OUTPUT}_rep0.csv
