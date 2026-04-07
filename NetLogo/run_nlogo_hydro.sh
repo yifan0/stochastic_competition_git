3eb139861cd276825a7ce311ce148947e4c02360
kodingkoning <koningelizabeth@gmail.com>
2026-04-07 12:27:37 -0500

    removed versions other than C and Julia DynamicGrids (NetLogo, Mesa, Julia, Numba, Repast4Py, Python)

--- NetLogo/run_nlogo_hydro.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=nlogo_sim    # Job name
-#SBATCH --time=0-05:20:00                 # Time limit hrs:min:sec
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --partition sandybridge
-
-time ~/NetLogo/NetLogo-6.3.0/NetLogo_Console --headless --model sim.nlogo --experiment experiment --table table.csv
-
